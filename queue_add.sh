56a49039983e5ad1e42ba1bbb07bf8c46b3dbbe9
Rakesh Kumar <gopchandani@gmail.com>
2015-04-15 08:54:45 -0700

    changing the bw

--- queue_add.sh
+++ queue_add.sh
@@ -1,3 +1,3 @@
-sudo ovs-vsctl -- set Port s1-eth1 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000000 queues=0=@q0 -- --id=@q0 create Queue other-config:min-rate=4000000 other-config:max-rate=4000000
-sudo ovs-vsctl -- set Port s1-eth2 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000000 queues=0=@q0 -- --id=@q0 create Queue other-config:min-rate=4000000 other-config:max-rate=4000000
-sudo ovs-vsctl -- set Port s1-eth3 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000000 queues=0=@q0 -- --id=@q0 create Queue other-config:min-rate=4000000 other-config:max-rate=4000000
+sudo ovs-vsctl -- set Port s1-eth1 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000000 queues=0=@q0 -- --id=@q0 create Queue other-config:min-rate=7000000 other-config:max-rate=7000000
+sudo ovs-vsctl -- set Port s1-eth2 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000000 queues=0=@q0 -- --id=@q0 create Queue other-config:min-rate=7000000 other-config:max-rate=7000000
+sudo ovs-vsctl -- set Port s1-eth3 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000000 queues=0=@q0 -- --id=@q0 create Queue other-config:min-rate=7000000 other-config:max-rate=7000000
